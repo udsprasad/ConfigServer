622853a4f2f4e4fad076e37b60dfc4e4d49b3679
durga satya Prasad <dspuppuluri@gmail.com>
2022-01-26 16:18:12 +0530

    initial commit

---
+++ src/test/java/com/example/ConfigSever/ConfigSeverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ConfigSever;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigSeverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
